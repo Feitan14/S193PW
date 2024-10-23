ec07c0bf0bfb27c0d79b6ce7163ed21f4fe85ca6
AndresMorales13 <andy38morales@gmail.com>
2024-10-22 20:31:39 -0600

    practica 6
    hasta el momento de la practica 6

--- introLaravl/app/Http/Controllers/controladorVistas.php
+++ introLaravl/app/Http/Controllers/controladorVistas.php
@@ -22,13 +22,21 @@ class controladorVistas extends Controller
     }
     public function procesarCliente(request $peticion)
     {
-       /* return 'si llego la info del cliente :)'; 
-        return $peticion->all();*/
-
-        //peticion de url del cliente
-        //return $peticion->url();
-        
-        //peticion ip del url de origen
-        return $peticion->ip();
+       //respuesta de redireccion
+       // usando la ruta
+       // return redirect('/');
+       // respuesta con redirect usando en nombre de la ruta
+      // return redirect()->route('rutaCliente');
+      //se utiliza la siguiente linea para mandar al origen de la peticion
+      // return back();
+      // arreglos
+      // redireccion de variable
+/*    $id= [['usuario'=>1],['usuario'=>2]];
+    return view('formulario', compact('id')); */
+    // redireccion de variable por session 
+    $usuario= $peticion->input('txtnombre');
+    session()->flash('exito','se guardo el usuario: '.$usuario);
+    //return con to route
+    return to_route('rutaform');
     }
 }
