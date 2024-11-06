fcb470f3cd0c3ed0bb4489f665adddf03c1a631c
AndresMorales13 <andy38morales@gmail.com>
2024-11-05 19:50:29 -0600

    primer comit
    primer comit examen

---
+++ examen/resources/views/components/layouts/plantilla.blade.php
@@ -0,0 +1,16 @@
+@extends('Layouts.plantilla')
+@section('titulo','componentes Blade')
+@section('contenido')
+
+<x-Card encabezado="componente" titulo="Dinamico" textoBoton="Guardar"> 
+    y
+</x-Card>
+<x-Card encabezado="componente2" titulo="Dinamico2" textoBoton="No Guardar"> 
+    x
+</x-Card>
+<x-Alert tipo="danger"> </x-Alert>
+<x-Alert tipo="warning"> </x-Alert>
+
+
+
+@endsection
